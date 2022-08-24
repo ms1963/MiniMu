a5a714081abc436d59cbcaa4fc65d79a13a22485
Michael Stal <michael.stal@gmail.com>
2022-08-24 17:21:09 +0200

    Update EvalVisitor.java
    Deleted unused List-import

--- src/main/java/minimu/EvalVisitor.java
+++ src/main/java/minimu/EvalVisitor.java
@@ -3,7 +3,6 @@ package minimu;
 import org.antlr.v4.runtime.misc.NotNull;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class EvalVisitor extends MiniMuBaseVisitor<Value> {
